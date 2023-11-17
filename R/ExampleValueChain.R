8f74cdfde7056d3d4aa43fc6b37bc251b8a7cf4b
ChristiaanPieterse <23576278@sun.ac.za>
2023-11-17 11:16:16 +0200

    Upload R package

---
+++ R/ExampleValueChain.R
@@ -0,0 +1,12 @@
+#'Example of an 'Automotive Value Chain'.
+#'
+#' A data set containing the categories for the automotive industry.
+#'
+#' @format A data frame with 175 rows and 3 variables:
+#' \describe{
+#'   \item{tierNumber}{Tier number of product}
+#'   \item{GVCactivityNumber}{Activity number of product}
+#'   \item{HScode}{Exporter, HS17 product code}
+#' }
+#' @source \url{https://www.unido.org/sites/default/files/2009-12/Global_automotive_industry_value_chain_0.pdf}
+"ExampleValueChain"
