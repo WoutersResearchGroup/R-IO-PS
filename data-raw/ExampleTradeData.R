87f09147fe4538ace9c0936fa6bc43dec6768749
ChristiaanPieterse <128793994+ChristiaanPieterse@users.noreply.github.com>
2024-01-06 13:36:40 +0200

    Add files via upload

---
+++ data-raw/ExampleTradeData.R
@@ -0,0 +1,7 @@
+## code to prepare `ExampleTradeData` dataset goes here
+
+ReadTradeData <-read.csv("BACI_HS17_Y2020_V202301.csv")
+#ExampleTradeData<-ReadTradeData[ReadTradeData$i == c(4, 710),]
+ExampleTradeData <- ReadTradeData[ReadTradeData$i %in% c(4, 710) & nchar(as.character(ReadTradeData$k)) == 6, ][c(FALSE, TRUE, FALSE, FALSE), ][c(FALSE, TRUE, FALSE, FALSE), ][c(FALSE, TRUE, FALSE, FALSE), ][c(FALSE, TRUE, FALSE, FALSE), ]
+
+usethis::use_data(ExampleTradeData, overwrite = TRUE, compress = "xz")
